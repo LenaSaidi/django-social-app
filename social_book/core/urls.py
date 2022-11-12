391ba7b2b28219ce9176db7e4b44e07b80fc544d
Lenasaidi <saidilena6@gmail.com>
2022-11-12 23:33:30 +0100

    first commit

---
+++ social_book/core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+#from django.views.generic.base import TemplateView
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('settings', views.settings, name='settings'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('logout', views.logout, name='logout'),
+]
